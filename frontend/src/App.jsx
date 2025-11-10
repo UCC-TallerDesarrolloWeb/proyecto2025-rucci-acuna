3a78882df91b5b022b9e8c3fb42f11ffcb78b145
Isabela Acuna <2405404@ucc.edu.ar>
2025-11-09 23:58:00 -0300

    otro commit con comentarios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,10 @@
+/**
+ * Componente principal de enrutamiento del proyecto BRÚJULA.
+ * ---------------------------------------------------------
+ * Define las rutas principales del sitio y aplica el Layout general
+ * que contiene el Header, Footer y el <Outlet> de React Router.
+ */
+
 import { Routes, Route } from "react-router-dom";
 import Layout from "@components/Layout";
 import Home from "@pages/Home";
@@ -9,7 +16,9 @@ export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route index element={<Home />} />              
+        {/* Ruta principal (Home) */}
+        <Route path="/" element={<Home />} />              
+        {/* Otras secciones */}
         <Route path="destinos" element={<Destinos />} />
         <Route path="itinerario" element={<Itinerario />} />
         <Route path="contacto" element={<Contacto />} />
@@ -17,4 +26,3 @@ export default function App() {
     </Routes>
   );
 }
-
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,14 @@
+/**
+ * Punto de entrada principal del proyecto BRÚJULA.
+ * Renderiza el componente App dentro de <HashRouter> para habilitar la navegación.
+ */
+
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom"; 
 import App from "./App.jsx";
 import "@styles/index.scss"; 
 import "./utils/funcionalidades.js";
-
-
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
